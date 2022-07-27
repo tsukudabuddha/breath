426cdf306951bbb1625adced5542ffeae911a82b
Andrew Tsukuda <andrewtsukuda@gmail.com>
2022-07-27 15:27:58 -0700

    handle cancel button press + move animation code to ProgressBar

--- components/Exercise/ExerciseView.tsx
+++ components/Exercise/ExerciseView.tsx
@@ -19,10 +19,10 @@ const progressBarWidth = Dimensions.get('screen').width * 0.5;
 
 export default function ExerciseView (props: Props) {
   const [innerSize] = useState(new Animated.Value(startingWidth));
-  const [progress] = useState(new Animated.Value(0));
   const [shouldAnimate, setShouldAnimate] = useState(false);
   const [selectedDurationIndex, setSelectedDurationIndex] = useState(0);
   const [exercise, setExercise] = useState(props.exercise);
+  const [progressBarValue, setProgressBarValue] = useState(0);
 
   let buttonText = "Start"
   let iterations = props.exercise.durationOptions[selectedDurationIndex];
@@ -62,13 +62,6 @@ export default function ExerciseView (props: Props) {
     )
     // Breathing animation
     Animated.loop(sequence, {iterations:iterations}).start(animationCallback);
-    const duration = (exercise.inhaleDuration + exercise.exhaleDuration) * iterations;
-    // Progress Bar animation
-    Animated.timing(progress, {
-      toValue: progressBarWidth, 
-      duration: duration, 
-      useNativeDriver: false
-    }).start();
   }
 
   if (shouldAnimate) {
@@ -76,13 +69,19 @@ export default function ExerciseView (props: Props) {
   } else {
     innerSize.stopAnimation();
     innerSize.setValue(startingWidth);
-    progress.setValue(0);
   }
 
   function didSelectHandler(exercise: ExerciseType) {
     setExercise(exercise);
     props.parentProps.navigation.pop();
   }
+
+  function startPauseButtonHandler() {
+    setProgressBarValue(0);
+    setShouldAnimate(!shouldAnimate);
+  }
+
+  const duration = (exercise.inhaleDuration + exercise.exhaleDuration) * iterations;
 
   return (
     <View style={styles.container}>
@@ -109,12 +108,12 @@ export default function ExerciseView (props: Props) {
         </Pressable>
       </View>
       {/* Progress Bar */}
-      <ProgressBar progress={progress} width={progressBarWidth}/>
+      <ProgressBar shouldAnimate={shouldAnimate} value={progressBarValue} style={{width: progressBarWidth}} duration={duration}/>
       {/* Duration Picker */}
       <DurationPicker style={styles.durationPicker} handlePress={handleDurationPickerPress} exercise={props.exercise} selectedIndex={selectedDurationIndex}/>
       {/* Start/Pause Button */}
       <View style={[styles.buttonContainer, styles.horizontalStack]}>
-        <Pressable style={[styles.button, styles.primary]} onPress={() => setShouldAnimate(!shouldAnimate)}>
+        <Pressable style={[styles.button, styles.primary]} onPress={startPauseButtonHandler}>
           <Text style={styles.text}>{buttonText}</Text>
         </Pressable>
         <Pressable style={[styles.button, styles.secondary]} onPress={() => props.parentProps.navigation.navigate('Modal', {
--- components/Shared/ProgressBar.tsx
+++ components/Shared/ProgressBar.tsx
@@ -1,15 +1,34 @@
-import { Animated, StyleSheet, View, Text } from 'react-native';
+import { useState } from 'react';
+import { Animated, StyleSheet, View, Text, ViewStyle } from 'react-native';
 type Props = {
-  progress: Animated.Value,
-  width?: number
+  style: ViewStyle,
+  shouldAnimate: boolean,
+  duration: number,
+  value: number,
 }
 
 export default function ProgressBar(props: Props) {
-  const additionalContainerStyling = {width: props.width ?? 100}
+  const [progress] = useState(new Animated.Value(0));
+  const [isAnimating, setIsAnimating] = useState(false);
+  if (typeof props.style.width !== 'number') {
+    return (<Text>Oops, an error has occurred</Text>)
+  }
+  const width = props.style.width ?? 100
+  if (!isAnimating && props.shouldAnimate) {
+    progress.setValue(props.value);
+    Animated.timing(progress, {
+      toValue: width, 
+      duration: props.duration, 
+      useNativeDriver: false
+    }).start();
+  } else {
+    progress.setValue(props.value);
+  }
+
   return (
-    <View style={[styles.container, additionalContainerStyling]}>
+    <View style={[styles.container, props.style]}>
       <View style={styles.totalProgress}>
-        <Animated.View style={[styles.progressBar, {width: props.progress}]}/>
+        <Animated.View style={[styles.progressBar, {width: progress}]}/>
       </View>
     </View>
   )
